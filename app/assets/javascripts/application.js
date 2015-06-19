afe2561bbc6c0ccbbce8b8402c49798309fc9c84
Laser Nite <lasernite@gmail.com>
2015-06-19 17:26:34 -0400

    added skrollr and intialized, test on page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require skrollr
+//= require skrollr.menu
+//= require skrollr.ie
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
